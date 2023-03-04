c4e0ff6e6b247c66168f9583dad89cbf6e1f6274
jksramos <jks.ramos@hotmail.com>
2023-03-03 21:55:38 -0300

    aula14 finalizada

--- aula14.py
+++ aula14.py
@@ -3,12 +3,19 @@ b = 'B'
 c = 1.1
 
 # Identifica o valor da {} pelo indice - a ordem importa
-formato_1 = 'a={} b={} c={}'.format(a, b, c)
+formato_1 = 'a={} b={} c={:.2f}'.format(a, b, c)
 
-string = 'a={} b={} c={}'
-formato_2 = string.format(a, b, c)
+string = 'a={} b={} c={:.2f}'
+formato_2 = string.format(a, b, c) # tbm pode ser escrito dessa forma
 
+# O indice está especificado nas {} - a ordem não faz diferença
+formato_3 = 'b={1} a={0} c={2:.2f}'.format(a, b, c)
 
+formato_4 = 'b={nome2} c={nome3:.2f} a={nome1}'.format(
+    nome1=a, nome2=b, nome3=c
+)
 
 print(formato_1)
 print(formato_2)
+print(formato_3)
+print(formato_4)
