2447202e6508faa48f18e43c2b449566fbd65538
jksramos <jks.ramos@hotmail.com>
2023-03-04 11:48:05 -0300

    04-03-2023 aulas 15 e 16

---
+++ aula15.py
@@ -0,0 +1,16 @@
+# nome = input('Qual o seu Nome? ')
+# print(f'O seu nome é: ')
+
+# Esse código vai funcionar, mas geraria um problema se o usuário digitar uma letra
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+
+print(f'A Soma dos dois números é: {numero_1 + numero_2}')
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A Soma dos dois números é: {int_numero_1 + int_numero_2}')
