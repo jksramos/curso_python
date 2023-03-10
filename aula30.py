cb27b350fec3bad126bab0d57ddc660513ec8a4a
jksramos <jks.ramos@hotmail.com>
2023-03-10 16:02:41 -0300

    10.03.2023 atualizando o curso

--- aula30.py
+++ aula30.py
@@ -4,8 +4,8 @@ Muitas condições no mesmo if (ruim)
     <- Contagem de complexidade (ruim)
 """
 
-velocidade = 61 # Velocidade atual do carro
-local_carro = 90 # Local em que o carro está na estrada
+velocidade =  None # Velocidade atual do carro
+local_carro = None # Local em que o carro está na estrada
 
 RADAR_1 = 60 # Velocidade máxima do radar 1
 LOCAL_1 = 100 # Local onde o radar 1 está
